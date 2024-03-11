0c8c1880576301c20749cff875a9428a753bbf26
00JCIV00 <jake.crawfordiv@gmail.com>
2024-03-11 16:52:14 -0400

    Improved Integer bounds checking with Saturated Operators.

--- src/widgets/border.zig
+++ src/widgets/border.zig
@@ -15,18 +15,18 @@ pub fn all(win: Window, style: Style) Window {
     const h = win.height;
     const w = win.width;
     win.writeCell(0, 0, .{ .char = top_left, .style = style });
-    win.writeCell(0, h - 1, .{ .char = bottom_left, .style = style });
-    win.writeCell(w - 1, 0, .{ .char = top_right, .style = style });
-    win.writeCell(w - 1, h - 1, .{ .char = bottom_right, .style = style });
+    win.writeCell(0, h -| 1, .{ .char = bottom_left, .style = style });
+    win.writeCell(w -| 1, 0, .{ .char = top_right, .style = style });
+    win.writeCell(w -| 1, h -| 1, .{ .char = bottom_right, .style = style });
     var i: usize = 1;
-    while (i < (h - 1)) : (i += 1) {
+    while (i < (h -| 1)) : (i += 1) {
         win.writeCell(0, i, .{ .char = vertical, .style = style });
-        win.writeCell(w - 1, i, .{ .char = vertical, .style = style });
+        win.writeCell(w -| 1, i, .{ .char = vertical, .style = style });
     }
     i = 1;
-    while (i < w - 1) : (i += 1) {
+    while (i < w -| 1) : (i += 1) {
         win.writeCell(i, 0, .{ .char = horizontal, .style = style });
-        win.writeCell(i, h - 1, .{ .char = horizontal, .style = style });
+        win.writeCell(i, h -| 1, .{ .char = horizontal, .style = style });
     }
-    return win.initChild(1, 1, .{ .limit = w - 2 }, .{ .limit = w - 2 });
+    return win.initChild(1, 1, .{ .limit = w -| 2 }, .{ .limit = w -| 2 });
 }
